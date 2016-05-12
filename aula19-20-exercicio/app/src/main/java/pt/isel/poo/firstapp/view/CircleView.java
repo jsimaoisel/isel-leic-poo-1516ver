e58a30cad6afb8df234eba5dd60b5b250c2c885c
jsimaoisel <jsimao@cc.isel.ipl.pt>
2016-05-12 19:14:38 +0100

    Aula 20: CircleView com debug de MotionEvents no método onTouchEvent

--- aula19-20-exercicio/app/src/main/java/pt/isel/poo/firstapp/view/CircleView.java
+++ aula19-20-exercicio/app/src/main/java/pt/isel/poo/firstapp/view/CircleView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class CircleView extends View {
@@ -22,13 +23,33 @@ public class CircleView extends View {
         invalidate();
     }
 
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.print("Down @");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.print("Up @ ");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.print("Move @ ");
+                break;
+        }
+        System.out.println(" x="
+                + ev.getX()
+                + " y=" + ev.getY());
+        return true;
+    }
+
     public void onDraw(Canvas c) {
         int cx = c.getWidth() / 2;
         int cy = c.getHeight() / 2;
+        paint.setColor(Color.DKGRAY);
         paint.setStyle(Paint.Style.FILL);
         c.drawCircle(cx, cy, radius, paint);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.GREEN);
+        paint.setColor(Color.RED);
         paint.setStrokeWidth(20);
         c.drawRect(1,1,getWidth()-1,getHeight()-1,paint);
     }
